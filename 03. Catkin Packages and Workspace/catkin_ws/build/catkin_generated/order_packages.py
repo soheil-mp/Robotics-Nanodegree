22e7e4943706078f6fe5784a7823cede4787c88a
Soheil Mohammadpour <Soheil.mpg@gmail.com>
2022-08-06 11:53:32 +0400

    update

--- 03. Catkin Packages and Workspace/catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/robond/Desktop/Robotics Software Engineer Nanodegree/03. Workspaces and Packages in Catkin/catkin_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/opt/ros/kinetic".split(';') if "/opt/ros/kinetic" != "" else []
--- 03. Catkin Packages and Workspace/catkin_ws/build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/robond/Desktop/Robotics Software Engineer Nanodegree/03. Workspaces and Packages in Catkin/catkin_ws/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
